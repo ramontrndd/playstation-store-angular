1572e271010c97f98e0184a10bc8ad88774a5929
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2023-10-02 14:25:31 -0300

    card-label: finish-

---
+++ psn-store-angular/src/app/components/card/card-price/card-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-price',
+  templateUrl: './card-price.component.html',
+  styleUrls: ['./card-price.component.css']
+})
+export class CardPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
